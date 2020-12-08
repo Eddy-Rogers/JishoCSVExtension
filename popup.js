f3a3729a23e8d8efa97cb1213f8848205bf4e3c3
NOLogicalSense <eddyrogersiv@gmail.com>
2020-12-08 13:56:00 -0500

    Separated the queries out

--- popup.js
+++ popup.js
@@ -6,8 +6,8 @@ chrome.storage.sync.get('color', function(data) {
 });
 
 changeColor.onclick = function(element) {
-    var csvResult
-    let color = element.target.value;
+
+    var result = "";
 
     // TODO :: Can we separate these queries?
 
@@ -18,38 +18,42 @@ changeColor.onclick = function(element) {
         runAt: 'document_start', // default is document_idle. See https://stackoverflow.com/q/42509273 for more details.
         }, function(results) {
           // results.length must be 1
-          var result = results[0].trim();
-
-          chrome.tabs.executeScript(null, {
-            // TODO :: What should happen here if no furigana are available? What about if there are okurigana?
-            code: furigana,
-            allFrames: false, // this is the default
-            runAt: 'document_start', // default is document_idle. See https://stackoverflow.com/q/42509273 for more details.
-            }, function(results) {
-              // results.length must be 1
-              if(!(results[0] === "")) {
-                result += ", [" + results[0] + "] ";
-              }
-              else {
-                result += ", ";
-              }
-
-              chrome.tabs.executeScript(null, {
-                code: meaning,
-                allFrames: false, // this is the default
-                runAt: 'document_start', // default is document_idle. See https://stackoverflow.com/q/42509273 for more details.
-                }, function(results) {
-                  // results.length must be 1
-                  result += results[0];
-                    navigator.clipboard.writeText(result).then(() => {
-                        //clipboard successfully set
-                      }, () => {
-                        //clipboard write failed, use fallback
-                      });
-                  });
-            });
+          result += results[0].trim();
         });
-    });
+      });
+
+    chrome.tabs.executeScript(null, {
+      // TODO :: What should happen here if no furigana are available? What about if there are okurigana?
+      code: furigana,
+      allFrames: false, // this is the default
+      runAt: 'document_start', // default is document_idle. See https://stackoverflow.com/q/42509273 for more details.
+      }, function(results) {
+        // results.length must be 1
+        if(!(results[0] === "")) {
+          result += ", [" + results[0] + "] ";
+        }
+        else {
+          result += ", ";
+        }
+
+
+      });
+
+    chrome.tabs.executeScript(null, {
+      code: meaning,
+      allFrames: false, // this is the default
+      runAt: 'document_start', // default is document_idle. See https://stackoverflow.com/q/42509273 for more details.
+      }, function(results) {
+        // results.length must be 1
+        result += results[0];
+
+        });
+
+        navigator.clipboard.writeText(result).then(() => {
+            //clipboard successfully set
+          }, () => {
+            //clipboard write failed, use fallback
+          });
   };
 
 // TODO :: I know this is a meme but there has to be a better way to do this.
