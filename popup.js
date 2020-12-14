62fc46aaf681b2c8c0b580b5ef6ea70168f7d4d1
Joseph Brooksbank <Joseph.Brooksbank@du.edu>
2020-12-14 17:30:38 -0600

    Cleaned up css and html formatting and styling; put buttons into a form and congregated all styling in stylesheet
    Converted to grid as well, for more dynamic positioning

--- popup.js
+++ popup.js
@@ -1,76 +1,72 @@
 let saveResult = document.getElementById("changeColor");
 
-chrome.storage.sync.get('color', function(data) {
-  saveResult.style.backgroundColor = data.color;
-  saveResult.setAttribute('value', data.color);
-});
 
-saveResult.addEventListener("click",function() {
-    // let color = element.target.value;
+saveResult.addEventListener("click", function () {
 
     // TODO :: Can we separate these queries?
 
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(null, {
-        code: kanji,
-        allFrames: false, // this is the default
-        runAt: 'document_start', // default is document_idle. See https://stackoverflow.com/q/42509273 for more details.
-        }, function(results) {
-          // results.length must be 1
-        let result = results[0].trim();
-        // Key for dictionary
-        let key = result;
-
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
         chrome.tabs.executeScript(null, {
-            // TODO :: What should happen here if no furigana are available? What about if there are okurigana?
-            code: furigana,
+            code: kanji,
             allFrames: false, // this is the default
             runAt: 'document_start', // default is document_idle. See https://stackoverflow.com/q/42509273 for more details.
-            }, function(results) {
-              // results.length must be 1
-              if(!(results[0] === "" || results[0] == null)) {
-                result += ", [" + results[0] + "] ";
-              }
-              else {
-                result += ", ";
-              }
+        }, function (results) {
+            // results.length must be 1
+            let result = results[0].trim();
+            // Key for dictionary
+            let key = result;
 
-              chrome.tabs.executeScript(null, {
-                code: meaning,
+            chrome.tabs.executeScript(null, {
+                // TODO :: What should happen here if no furigana are available? What about if there are okurigana?
+                code: furigana,
                 allFrames: false, // this is the default
                 runAt: 'document_start', // default is document_idle. See https://stackoverflow.com/q/42509273 for more details.
-                }, function(results) {
-                  // results.length must be 1
-                  result += results[0] + "\n";
-                  let storedResults = JSON.parse(window.localStorage.getItem("results"));
-                  if (storedResults === null) {
-                    storedResults = {};
-                  }
-                  storedResults[key] = result;
-                  window.localStorage.setItem("results", JSON.stringify(storedResults));
-                  });
+            }, function (results) {
+                // results.length must be 1
+                if (!(results[0] === "" || results[0] == null)) {
+                    result += ", [" + results[0] + "] ";
+                } else {
+                    result += ", ";
+                }
+
+                chrome.tabs.executeScript(null, {
+                    code: meaning,
+                    allFrames: false, // this is the default
+                    runAt: 'document_start', // default is document_idle. See https://stackoverflow.com/q/42509273 for more details.
+                }, function (results) {
+                    // results.length must be 1
+                    result += results[0] + "\n";
+                    let storedResults = JSON.parse(window.localStorage.getItem("results"));
+                    if (storedResults === null) {
+                        storedResults = {};
+                    }
+                    storedResults[key] = result;
+                    window.localStorage.setItem("results", JSON.stringify(storedResults));
+                });
             });
         });
     });
-  });
+});
 
 let sendToClip = document.getElementById("sendToClip");
-sendToClip.addEventListener("click", function(){
-  let results = JSON.parse(window.localStorage.getItem("results"));
-  let resultString = "";
-  for (const key in results){
-    // skip prototype keys
-    if (!results.hasOwnProperty(key)) continue;
-    resultString += results[key];
-  }
+sendToClip.addEventListener("click", function () {
+    let results = JSON.parse(window.localStorage.getItem("results"));
+    let resultString = "";
+    for (const key in results) {
+        // skip prototype keys
+        if (!results.hasOwnProperty(key)) continue;
+        resultString += results[key];
+    }
 
-  // clearing saved buffer
-  window.localStorage.setItem("results", JSON.stringify({}));
-  navigator.clipboard.writeText(resultString).then(() => {}, () => {});
+    // clearing saved buffer
+    window.localStorage.setItem("results", JSON.stringify({}));
+    navigator.clipboard.writeText(resultString).then(() => {
+    }, () => {
+    });
 });
 // Look into searching by element class name after getting the div 'primary'
 const furigana =
-`                  // array of spans, each containing either a section of the furigana or nothing (in case of a okurigana in the original word)
+    `                  // array of spans, each containing either a section of the furigana or nothing (in case of a okurigana in the original word)
                   let furiganaSpans = [];
                   // **SPECIAL CASE** Apparently sometimes jisho uses ruby to store the furigana, and also includes kanji?? Why
                   furiganaSpans[0] =  document.querySelector("#primary .furigana rt");
