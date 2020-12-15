cdb6f4811f2e32b43b6319d63d4c56e48e8dcbc9
Joseph Brooksbank <Joseph.Brooksbank@du.edu>
2020-12-15 17:37:08 -0600

    Deleted tutorial background.js code
    Added a line between the buttons and the list of definitions currently saved
    
    changed back from <form> to just a <div> containing the buttons; forms send a 'submit' command whenever a button presses; in our case that just refreshes the page. Since it isn't really a form, a <div> is fine and works better.
    
    Surrounded all JS code in an eventlistener for DOMContentLoadded, which will ensure none of our code is attempted to run before the webpage loads.
    
    Added actual definition list capability; now shows which definitions are saved on the right side of the window (basically, what will be sent to [output] -- currently clipboard.

--- popup.js
+++ popup.js
@@ -1,72 +1,88 @@
-let saveResult = document.getElementById("changeColor");
 
+// Making sure all content is loaded before we try to do anything with the extension
+document.addEventListener('DOMContentLoaded', function() {
 
-saveResult.addEventListener("click", function () {
+    updateList();
+    let saveResult = document.getElementById("changeColor");
 
-    // TODO :: Can we separate these queries?
+    saveResult.addEventListener("click", function () {
 
-    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-        chrome.tabs.executeScript(null, {
-            code: kanji,
-            allFrames: false, // this is the default
-            runAt: 'document_start', // default is document_idle. See https://stackoverflow.com/q/42509273 for more details.
-        }, function (results) {
-            // results.length must be 1
-            let result = results[0].trim();
-            // Key for dictionary
-            let key = result;
+        // TODO :: Can we separate these queries?
+        let definition = {}
 
+        chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
             chrome.tabs.executeScript(null, {
-                // TODO :: What should happen here if no furigana are available? What about if there are okurigana?
-                code: furigana,
+                code: kanji,
                 allFrames: false, // this is the default
                 runAt: 'document_start', // default is document_idle. See https://stackoverflow.com/q/42509273 for more details.
             }, function (results) {
+
                 // results.length must be 1
-                if (!(results[0] === "" || results[0] == null)) {
-                    result += ", [" + results[0] + "] ";
-                } else {
-                    result += ", ";
-                }
+                definition.kanji = results[0].trim();
+
 
                 chrome.tabs.executeScript(null, {
-                    code: meaning,
+                    // TODO :: What should happen here if no furigana are available? What about if there are okurigana?
+                    code: furigana,
                     allFrames: false, // this is the default
                     runAt: 'document_start', // default is document_idle. See https://stackoverflow.com/q/42509273 for more details.
                 }, function (results) {
                     // results.length must be 1
-                    result += results[0] + "\n";
-                    let storedResults = JSON.parse(window.localStorage.getItem("results"));
-                    if (storedResults === null) {
-                        storedResults = {};
+                    if (!(results[0] === "" || results[0] == null)) {
+                        definition.furigana = results[0];
+                    } else {
+                        definition.furigana = "";
                     }
-                    storedResults[key] = result;
-                    window.localStorage.setItem("results", JSON.stringify(storedResults));
+
+                    chrome.tabs.executeScript(null, {
+                        code: meaning,
+                        allFrames: false, // this is the default
+                        runAt: 'document_start', // default is document_idle. See https://stackoverflow.com/q/42509273 for more details.
+                    }, function (results) {
+                        // results.length must be 1
+                        definition.meaning = results[0];
+
+                        //storedResults is a JS object containing subobjects for every definition
+                        let storedResults = JSON.parse(window.localStorage.getItem("results"));
+                        if (storedResults === null) {
+                            storedResults = {};
+                        }
+                        storedResults[definition.kanji] = definition;
+                        window.localStorage.setItem("results", JSON.stringify(storedResults));
+                        updateList();
+
+                    });
                 });
             });
         });
     });
-});
 
-let sendToClip = document.getElementById("sendToClip");
-sendToClip.addEventListener("click", function () {
-    let results = JSON.parse(window.localStorage.getItem("results"));
-    let resultString = "";
-    for (const key in results) {
-        // skip prototype keys
-        if (!results.hasOwnProperty(key)) continue;
-        resultString += results[key];
-    }
 
-    // clearing saved buffer
-    window.localStorage.setItem("results", JSON.stringify({}));
-    navigator.clipboard.writeText(resultString).then(() => {
-    }, () => {
+
+    let sendToClip = document.getElementById("sendToClip");
+    sendToClip.addEventListener("click", function () {
+        let results = JSON.parse(window.localStorage.getItem("results"));
+        let resultString = "";
+        for (const key in results) {
+            // skip prototype keys
+            if (!results.hasOwnProperty(key)) continue;
+            let definition = results[key];
+            resultString += definitionString(definition) + "\n";
+
+        }
+        navigator.clipboard.writeText(resultString).then(() => {
+        }, () => {
+        });
+
+        // clearing saved buffer
+        window.localStorage.setItem("results", JSON.stringify({}));
+        // clear list in extension window
+        updateList();
+
     });
-});
 // Look into searching by element class name after getting the div 'primary'
-const furigana =
-    `                  // array of spans, each containing either a section of the furigana or nothing (in case of a okurigana in the original word)
+    const furigana =
+        `                  // array of spans, each containing either a section of the furigana or nothing (in case of a okurigana in the original word)
                   let furiganaSpans = [];
                   // **SPECIAL CASE** Apparently sometimes jisho uses ruby to store the furigana, and also includes kanji?? Why
                   furiganaSpans[0] =  document.querySelector("#primary .furigana rt");
@@ -91,7 +107,32 @@ const furigana =
                       result += f.textContent;
                     }
                   } result;`
-const kanji = `document.querySelector("#primary .text").textContent;`
+    const kanji = `document.querySelector("#primary .text").textContent;`
 
 // TODO :: Get all of the meanings and separate them?
-const meaning = `document.querySelector("#primary .meaning-meaning").textContent`
+    const meaning = `document.querySelector("#primary .meaning-meaning").textContent`
+});
+
+/**
+ * Updates the list of items in the queue
+ */
+function updateList() {
+    let results = JSON.parse(window.localStorage.getItem("results"));
+    // clearing current list
+    document.getElementById("currentQueue").innerHTML = "";
+    for (const key in results) {
+        // skip prototype keys
+        if (!results.hasOwnProperty(key)) continue;
+        let definition = results[key];
+        let templi = document.createElement("li");
+        templi.textContent = definitionString(definition);
+        document.getElementById("currentQueue").appendChild(templi);
+    }
+
+
+}
+
+function definitionString(definition) {
+    return definition.kanji + ", [" + definition.furigana + "] " + definition.meaning;
+}
+
